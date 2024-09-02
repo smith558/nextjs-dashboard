756324203e1ddb482c491df2fd000db8a01eba7b
Stanislav Modrak <stanislav.modrak@gmail.com>
2024-09-02 19:55:13 +0100

    Next steps

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,8 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
 import React from "react";
 
+export const experimental_ppr = true;
+
 export default function Layout({children}: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
@@ -8,6 +10,7 @@ export default function Layout({children}: { children: React.ReactNode }) {
         <SideNav/>
       </div>
       <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+
     </div>
   );
 }
